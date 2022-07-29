950486c2f4a05be9a20f257878f07003d298c1ac
mysmah <mysmah1024@gmail.com>
2022-07-29 08:20:40 +0700

    todo: fill enums and implement all the logic

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::fmt;
+
+enum Sounds {}
+
+enum RuPhonemes {}
+
+struct Sound {
+    sound: Sounds,
+    is_palatalized: bool,
+    is_long: bool,
+}
+
+struct RuPhoneme {
+    phoneme: RuPhonemes,
+    is_palatalized: bool,
+}
+
+pub struct IPA(Vec<Sound>);
+
+impl IPA {
+    pub fn new(ipa: String) -> Self {
+        todo!()
+    }
+
+    fn get(self) -> std::vec::IntoIter<Sound> {
+        self.0.into_iter()
+    }
+}
+
+pub struct RuPhonemeSec(Vec<RuPhoneme>);
+
+impl RuPhonemeSec {
+    pub fn new(ipa: IPA) -> Self {
+        ipa.get().fold(Self::default(), Self::next)
+    }
+
+    fn next(self, sound: Sound) -> Self {
+        todo!()
+    }
+}
+
+impl Default for RuPhonemeSec {
+    fn default() -> Self {
+        todo!()
+    }
+}
+
+impl fmt::Display for RuPhonemeSec {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+mod lib;
+
 use clap::Parser;
+use lib::{RuPhonemeSec, IPA};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     #[clap(value_parser)]
-    ipa: String
+    ipa: String,
 }
 
 fn main() {
     let ipa = Cli::parse().ipa;
 
-    println!("{}", ipa);
+    let parsed_ipa = IPA::new(ipa);
+
+    println!("{}", RuPhonemeSec::new(parsed_ipa));
 }
