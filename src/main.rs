470865916615b21773c9e0ce4c913fc5cdde324d
mysmah <mysmah1024@gmail.com>
2022-07-29 07:35:43 +0700

    cli using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(value_parser)]
+    ipa: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ipa = Cli::parse().ipa;
+
+    println!("{}", ipa);
 }
